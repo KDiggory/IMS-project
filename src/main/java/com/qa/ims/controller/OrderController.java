42c71b6755d232b3ea2a42ce375118aa73381447
KDiggory <katie.diggory@gmail.com>
2021-11-22 21:51:56 +0000

    Order Controller class

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,74 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.dao.OrderItemDAO;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order>{
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private OrderDAO orderDAO;
+	private CustomerDAO customerDAO;
+	private ItemDAO itemDAO;
+	private Utils utils;
+	private OrderItemDAO orderItemDAO;
+
+	public OrderController(OrderDAO orderDAO, Utils utils) {
+		super();
+		this.orderDAO = orderDAO;
+		this.utils = utils;
+	}
+	
+
+	@Override
+	public List<Order> readAll() {
+		List<Order> orders = orderDAO.readAll();
+		for (Order order : orders) {
+			LOGGER.info(order);
+		}
+		return orders;
+	}
+
+	@Override
+	public Order create() {
+		LOGGER.info("Please enter the customer id");
+		Long customerId = utils.getLong();
+		Order order = orderDAO.create(new Order(customerId));
+		LOGGER.info("Order created");
+		OrderItemController orderItemController = new OrderItemController(orderItemDAO, utils);
+		System.out.println("Adding items to the order");
+		(orderItemController).create();
+		return order;
+	}
+
+
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long id = utils.getLong();
+		return customerDAO.delete(id);
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Order readLatest() {
+		OrderDAO orderDAO = new OrderDAO();
+		Order order = orderDAO.readLatest();
+		return order;
+		
+	}
+
+}
