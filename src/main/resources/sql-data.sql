9eb9c4290010abea73e60c1258417554aab45563
KDiggory <katie.diggory@gmail.com>
2021-11-22 21:31:11 +0000

    PROJ-7 #comment schemas and test info for sql db

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,7 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+
+INSERT INTO `items` (`name`,`size`, `cost`) VALUES ('beer', 'very large', 5);
+
+INSERT INTO `ims`.`orders` (`customerId`) VALUES (1);
+
+INSERT INTO `ims`.`order_items` (`orderId`, `itemId`, `itemName`, `cost`) VALUES (1,1, ' beer', 5);
